be266ec189660dd0eb5191c4bbdd6b58bf5ec793
edenduthie <eduthie@gmail.com>
2014-01-25 05:24:01 +0000

    Lease.java

---
+++ src/main/java/nometry/service/consensus/Lease.java
@@ -0,0 +1,20 @@
+package nometry.service.consensus;
+
+public class Lease
+{
+    long nodeId;
+    long timestamp;
+    
+	public long getNodeId() {
+		return nodeId;
+	}
+	public void setNodeId(long nodeId) {
+		this.nodeId = nodeId;
+	}
+	public long getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(long timestamp) {
+		this.timestamp = timestamp;
+	}
+}
