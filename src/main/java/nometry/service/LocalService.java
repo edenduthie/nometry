46d09956a687cac7ed11b072ab5627c6e32623c1
edenduthie <eduthie@gmail.com>
2014-01-26 05:24:48 +0000

    LocalService.java

---
+++ src/main/java/nometry/service/LocalService.java
@@ -0,0 +1,17 @@
+package nometry.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+/**
+ * These services are local to a single node and are triggered by network communication with the node. 
+ * These services are sub-services of the larger global services. 
+ * Requests must be authenticated as coming from another node in the cluster.
+ * 
+ * @author eduthie
+ *
+ */
+public class LocalService 
+{
+
+}
