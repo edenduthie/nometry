66b26d24481dd0965e87b29171d1039fe7a7c401
edenduthie <eduthie@gmail.com>
2014-01-30 05:23:58 +0000

    GlobalService.java

---
+++ src/main/java/nometry/service/GlobalService.java
@@ -0,0 +1,16 @@
+package nometry.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+/**
+ * These services can be performed by any node and require communication between the node and all other nodes in the cluster to complete the required actions. 
+ * These services perform authentication and authorisation and do not need to originate from inside the cluster. 
+ * 
+ * @author eduthie
+ *
+ */
+public class GlobalService 
+{
+
+}
