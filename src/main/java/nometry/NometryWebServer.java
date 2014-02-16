f31c0b98b269099a0866745453ad24d19944bc1a
edenduthie <eduthie@gmail.com>
2014-02-16 05:19:02 +0000

    NometryWebServer.java

---
+++ src/main/java/nometry/NometryWebServer.java
@@ -0,0 +1,11 @@
+package nometry;
+
+/**
+ * The NometryWebServer responds to JSON requests via http/s, these are routed to the NometryServer
+ * @author eduthie
+ *
+ */
+public class NometryWebServer
+{
+
+}
