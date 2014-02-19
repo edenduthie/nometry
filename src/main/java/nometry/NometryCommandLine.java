0b7a9fe7ebbef5df15cc62b768242d69ae3552a1
edenduthie <eduthie@gmail.com>
2014-02-19 05:17:15 +0000

    NometryCommandLine.java

---
+++ src/main/java/nometry/NometryCommandLine.java
@@ -0,0 +1,12 @@
+package nometry;
+
+/**
+ * Responds to command line requests and calls the appropriate NometryServer service
+ * 
+ * @author eduthie
+ *
+ */
+public class NometryCommandLine
+{
+
+}
